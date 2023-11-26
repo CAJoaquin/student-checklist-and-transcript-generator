1f5758817b55c36c1e496f1caedb08b3b94ec66a
Charles Joaquin <cpjoaquin@up.edu.ph>
2023-11-26 14:48:05 +0800

    Add student_checklist_and_transcript_generator.c

---
+++ student_checklist_and_transcript_generator.c
@@ -0,0 +1,22 @@
+/*********************************************************************************************
+ 
+file name: student_checklist_and_transcript_generator.c
+
+----------------------------------------------------------------------------------------------
+ 
+Purpose: Generate either a semestral GWA report, a full checklist, or an overall transcript.
+Author: Charles Ansbert P. Joaquin
+Creation Date: 10 November 2023
+ 
+Instructions: 
+    The “Student Checklist and Transcript Generator” is a system that allows a single user
+    to input a student’s grades obtained for one or more semesters, with the option to
+    generate either a semestral GWA report, a full checklist, or an overall transcript.
+ 
+**********************************************************************************************/
+#include <stdio.h>
+
+int main(void) 
+{
+    return 0;
+}
